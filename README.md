59c855e966a662de6dd7460155c34056d155c0b4
sonichy <sonichy@163.com>
2020-06-15 16:59:00 +0800

    增加排序和过滤

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 海天鹰SQL
 一款基于 Qt5 的SQL数据库管理工具，已编译好的 HTYSQL 适用于 Linux 64 位 Qt5.11 环境。
-![alt](preview.png)
+![alt](preview.jpg)
 ### 参考
 [Qt SQL Browser](https://doc.qt.io/archives/qt-5.11/qtsql-sqlbrowser-example.html)  
 HeidiSQL  
--- mainwindow.h
+++ mainwindow.h
@@ -5,6 +5,7 @@
 #include <QTreeWidgetItem>
 #include <QSqlDatabase>
 #include <QSqlTableModel>
+#include <QSettings>
 
 namespace Ui {
 class MainWindow;
@@ -20,14 +21,20 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QSettings settings;
     QSqlDatabase db;
     QSqlTableModel *tableModel;
     void genTree();
+    void readSettings();
+
+protected:
+    void closeEvent(QCloseEvent *event);
 
 private slots:
     void on_action_add_connection_triggered();
     void on_action_Qt_triggered();
     void on_action_about_triggered();
+    void on_pushButton_filter_clicked();
     void on_pushButton_query_clicked();
     void on_treeWidget_customContextMenuRequested(QPoint point);
     void itemActivated(QTreeWidgetItem *item, int column);
